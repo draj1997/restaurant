3c398691af55cb0a6b97a2653a2944d4b9a39436
Devendra <drajcommerce@gmail.com>
2021-03-30 12:04:56 +0530

    First commit

---
+++ Student/includes/footer.php
@@ -0,0 +1,40 @@
+<div id="footer" class="cf">
+
+<div class="column three">
+     <strong>Phone</strong>
+     9934208755
+</div><!--column--> 
+
+<div class="column three">
+        <strong>Location</strong>
+        KOrmangala Bengaluru
+</div><!--column-->
+
+<div class="column three last">
+<strong>Hours</strong>
+<em>Tuesday - Thursday </em><br>
+1:00pm - 9:00pm<br><br>
+
+<em>friday - Saturday </em><br>
+4:00pm - 11:00pm<br><br>
+
+<em>Sunday - Monday </em><br>
+closed<br><br>
+
+<?php include('includes/store-hours.php'); ?>
+
+</div><!--column-->
+
+
+</div><!--footer-->
+
+<small>&copy; <?php echo date('Y');?> <?php echo $companyName; ?></small>
+
+
+</div> <!--wrapper-->
+
+<div class= "copyright-info">
+<?php include('../assests/includes/copyright.php'); ?>
+
+</div><!--copyright--> 
+</body></html>
---
+++ Student/index.php
@@ -0,0 +1,18 @@
+<?php
+define("TITLE", "Home | Dev's Fine Dining");
+include('includes/header.php');
+?>
+<div id="philosophy">
+		<hr>
+		
+		<h1>Dev's Philosophy of Fine Dining</h1>
+		<p>Here at Dev's, we know that good food isn't just about how expensive the dish is. We're not pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
+		<p>Oh, and you can call us DevOps. You're family here.</p>
+		
+		<hr>
+	</div><!-- philosophy -->
+
+<?php
+include('includes/footer.php');
+?>
+  
